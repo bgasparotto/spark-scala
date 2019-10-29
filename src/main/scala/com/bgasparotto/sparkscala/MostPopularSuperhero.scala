e6da9a020e97d1fd8c1bf4cbfa4866041f8744b1
bgasparotto <bruno.m.gasparotto@gmail.com>
2019-10-29 21:42:01 +0100

    Added a few explanations

--- src/main/scala/com/bgasparotto/sparkscala/MostPopularSuperhero.scala
+++ src/main/scala/com/bgasparotto/sparkscala/MostPopularSuperhero.scala
@@ -8,13 +8,13 @@ object MostPopularSuperhero {
 
   // Function to extract the hero ID and number of connections from each line
   def countCoOccurrences(line: String): (Int, Int) = {
-    val elements = line.split("\\s+")
+    val elements = line.split("\\s+") // Splits the line by any amount of spaces
     (elements(0).toInt, elements.length - 1)
   }
 
   // Function to extract hero ID -> hero name tuples (or None in case of failure)
   def parseNames(line: String): Option[(Int, String)] = {
-    val fields = line.split('\"')
+    val fields = line.split('\"') // Splits the line based on the quotation mark
     if (fields.length > 1) {
       Some(fields(0).trim().toInt, fields(1))
     } else {
@@ -33,12 +33,17 @@ object MostPopularSuperhero {
 
     // Build up a hero ID -> name RDD
     val names = sc.textFile("src/main/resources/dataset/marvel-graph/Marvel-names.txt")
+
+    /*
+     * Flat map is used because a few lines will be invalid and return None. In that case, the amount of records
+     * actually processed will differ than the amount of lines in the file.
+     */
     val namesRdd = names.flatMap(parseNames)
 
     // Load up the superhero co-apperarance data
     val lines = sc.textFile("src/main/resources/dataset/marvel-graph/Marvel-graph.txt")
 
-    // Convert to (heroID, number of connections) RDD
+    // Convert to (heroID, number of connections) RDD. All rows will be processed so map is used.
     val pairings = lines.map(countCoOccurrences)
 
     // Combine entries that span more than one line
@@ -52,12 +57,13 @@ object MostPopularSuperhero {
 
     // Look up the name (lookup returns an array of results, so we need to access the first result with (0)).
     val mostPopularName = namesRdd.lookup(mostPopular._2).head
+    val mostPopularId = mostPopular._2
 
     // Print out our answer!
-    println(s"$mostPopularName is the most popular superhero with ${mostPopular._1} co-appearances.")
+    println(s"$mostPopularName (id $mostPopularId) is the most popular superhero with ${mostPopular._1} co-appearances.")
 
     // Top 10 most populars
-    val tenMostPopular = flipped.sortBy(- _._1).take(10)
+    val tenMostPopular = flipped.sortBy(-_._1).take(10)
     println("10 most popular superheroes")
     tenMostPopular.foreach(hero => println(namesRdd.lookup(hero._2).head))
 
